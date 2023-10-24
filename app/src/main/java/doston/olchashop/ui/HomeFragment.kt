9fdb9d8ab0bccb0a560d922959c2a46e2e5e3a6c
DOSTONboy <89970820+dhusniddinov123@users.noreply.github.com>
2023-10-24 06:03:58 +0500

    7

--- app/src/main/java/doston/olchashop/ui/HomeFragment.kt
+++ app/src/main/java/doston/olchashop/ui/HomeFragment.kt
@@ -43,6 +43,7 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
