bc5d5db23228261fcd65b62192551dad5ad7a1bf
DOSTONboy <89970820+dhusniddinov123@users.noreply.github.com>
2023-10-25 05:48:48 +0500

    9

--- app/src/main/java/doston/olchashop/MainActivity.kt
+++ app/src/main/java/doston/olchashop/MainActivity.kt
@@ -2,8 +2,11 @@ package doston.olchashop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import doston.olchashop.databinding.ActivityMainBinding
+import doston.olchashop.ui.CartFragment
 import doston.olchashop.ui.HomeFragment
+import doston.olchashop.ui.ProfileFragment
 import doston.olchashop.ui.SplashFragment
 
 class   MainActivity : AppCompatActivity() {
@@ -11,6 +14,39 @@ class   MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportFragmentManager.beginTransaction().add(R.id.main, SplashFragment()).commit()
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.bag -> replaceFragment(CartFragment())
+                R.id.account -> replaceFragment(ProfileFragment())
+
+                else ->{
+
+
+
+                }
+
+            }
+
+            true
+
+        }
+
+    }
+    private fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
+
+
     }
 }
+
+//        supportFragmentManager.beginTransaction().add(R.id.main, SplashFragment()).commit()
